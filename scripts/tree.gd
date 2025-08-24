d57ae05d202b299e3009f913ae295fb3a61cab53
BossIion <ulysse.schaff@gmail.com>
2025-08-24 13:22:08 -0400

    Tree fall

---
+++ scripts/tree.gd
@@ -0,0 +1,28 @@
+extends Area3D
+
+# A variable to hold a reference to the axe node when it's detected
+var axe_in_range = false
+
+func _ready():
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(node):
+	# This signal is only called when a body enters the area.
+	print("area entered")
+	if node.name == "Axe":
+		print("Axe detected")
+		axe_in_range = true
+
+func _on_area_exited(node):
+	# It's good practice to also handle when a body exits the area.
+	if node.name == "Axe":
+		print("Axe exited")
+		axe_in_range = false
+
+func _process(_delta):
+	# Check for input every frame, but only if the axe is in range.
+	if axe_in_range:
+		if Input.is_action_just_pressed("click"):
+			print("clicked")
+			# Assuming the AnimationPlayer is a child of the current node
+			$AnimationPlayer.play("Tree_Fall")
